6f88e6a2a3cf7050a8b451857d274e744760f845
Henrique Baron <henrique.baron@gmail.com>
2022-04-03 17:28:38 +0200

    Show different selector types for a component

--- app/src/app/servers/servers.component.ts
+++ app/src/app/servers/servers.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // The component selector works like a CSS selector.
+  // Therefore, there are many ways to declare how a component should be called in the HTML file.
+  selector: 'app-servers', // As a specific HTML tag, the most common way to go
+  // selector: '[app-servers]', // As an HTML attritbute
+  // selector: '.app-servers', // As an HTML class
   // The template (intead of templateUrl) allows the input of the HTML code
   // directly on the TypeScript file.
   // The backtick string is a JavaScript feature that allows the input of 
