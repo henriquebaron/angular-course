453c6871de8064c67c84227c2629b120e9ef865e
Henrique Baron <henrique.baron@gmail.com>
2022-04-15 11:05:35 +0200

    Assignment 3: Solve exercise

--- basics-assignment-3/src/app/app.component.ts
+++ basics-assignment-3/src/app/app.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { withLatestFrom } from 'rxjs-compat/operator/withLatestFrom';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    .changeColor {
+      color: white;
+    }
+  `]
 })
 export class AppComponent {
+  detailsVisible: boolean = false;
+  entries: number[] = [];
+  entryCount: number = 0;
+
+  onDisplayDetails(): void {
+    this.detailsVisible = !this.detailsVisible;
+    this.entryCount++;
+    this.entries.push(this.entryCount);
+  }
+
+  getBackgroundColor(entry: number): string {
+    if (entry >= 5) return 'blue';
+    else return 'transparent';
+  }
 }
