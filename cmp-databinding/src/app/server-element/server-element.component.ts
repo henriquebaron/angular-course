dc23fe6e60d0627787b9ca58f4a8ab88b61f91ac
Henrique Baron <henrique.baron@gmail.com>
2022-04-19 22:49:55 +0200

    Comp. and Databinding: Alias to @Input decorator

--- cmp-databinding/src/app/server-element/server-element.component.ts
+++ cmp-databinding/src/app/server-element/server-element.component.ts
@@ -7,7 +7,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
   // The @Input() decorator is necessary to expose a property of the component to other components
-  @Input() element: {type: string, name: string, content: string};
+  // An alias can be given between the parentheses
+  @Input('srvElement') element: {type: string, name: string, content: string};
 
   constructor() { }
 
