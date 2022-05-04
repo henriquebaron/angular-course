6a091e2ee3aa8a87970f17f6c6b3d1112b8272cf
Henrique Baron <henrique.baron@gmail.com>
2022-05-04 05:18:36 +0200

    Implement navigation between features
    Through the navigation bar on the header.

--- recipes-app/src/app/app.component.ts
+++ recipes-app/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipes-app';
+  currentPage: string = 'recipes';
+
+  onNavigate(page: string): void {
+    this.currentPage = page;
+  }
 }
--- recipes-app/src/app/header/header.component.ts
+++ recipes-app/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,11 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() navigated = new EventEmitter<string>();
   collapsed: boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onNavigate(page: string): void {
+    this.navigated.emit(page);
   }
 
 }
