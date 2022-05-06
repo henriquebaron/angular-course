97db1c2cc9022b65a38c3c6fd8684c09eace7e18
Henrique Baron <henrique.baron@gmail.com>
2022-05-06 21:53:34 +0200

    HostBinding decorator to bind to template element properties

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,9 +1,12 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  // The HostBinding decorator binds to a property of the element using the directive.
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
@@ -13,12 +16,15 @@ export class BetterHighlightDirective implements OnInit {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
   }
 
+  // The HostListener decorator listents to an event from the element using the directive.
   @HostListener('mouseenter') mouseHover(eventData: Event) { 
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = 'blue';
   }
 
   @HostListener('mouseleave') mouseLeave(eventData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
 
 }
