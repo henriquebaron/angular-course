ca56b6fa8795d073a663e75e1c19200a0202ebe5
Henrique Baron <henrique.baron@gmail.com>
2022-05-06 22:00:11 +0200

    Configure a custom directive with property binding
    Which works just as in regular components.

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,11 +1,14 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
+import { defaultCoreCipherList } from 'constants';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue';
   // The HostBinding decorator binds to a property of the element using the directive.
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
@@ -14,17 +17,18 @@ export class BetterHighlightDirective implements OnInit {
     Using the renderer is the best way to access the DOM. In some cases, like services, for example,
     acessing the element directly would not even work. */
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.defaultColor;
   }
 
   // The HostListener decorator listents to an event from the element using the directive.
   @HostListener('mouseenter') mouseHover(eventData: Event) { 
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseLeave(eventData: Event) {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 
 }
