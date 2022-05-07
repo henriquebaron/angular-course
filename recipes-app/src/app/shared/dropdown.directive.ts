dd49e36f902023987ac3b1f67048db4ca22e2e8c
Henrique Baron <henrique.baron@gmail.com>
2022-05-07 12:42:23 +0200

    Improve the behavior of the dropdown directive
    By closing the dropdown by clicking elsewhere on the document.

--- recipes-app/src/app/shared/dropdown.directive.ts
+++ recipes-app/src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
@@ -6,10 +6,12 @@ import { Directive, HostBinding, HostListener } from '@angular/core';
 export class DropdownDirective {
   @HostBinding('class.open') open = false;
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) { }
 
-  @HostListener('click') toggleOpen(eventData: Event): void {
-    this.open = !this.open;
+  @HostListener('document:click', ['$event']) toggleOpen(eventData: Event): void {
+    /* Enables the dropdown to be closed by clicking elsewhere on the page. Now the listener
+    listens to clicks on the document (not on the element) and checks if the click was on the element. */
+    this.open = this.elementRef.nativeElement.contains(eventData.target) ? !this.open : false;
   }
 
 }
