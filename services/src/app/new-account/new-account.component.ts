5b6545aedd816db7463aed21e08e2783da2f7a05
Henrique Baron <henrique.baron@gmail.com>
2022-05-07 18:52:03 +0200

    Create a new service and call it from a component

--- services/src/app/new-account/new-account.component.ts
+++ services/src/app/new-account/new-account.component.ts
@@ -1,18 +1,25 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  // To use a service properly, it must be added tot he providers of the component
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
+
+  // Dependency Injection must also be used to let Angular instantiate the service for the component.
+  constructor(private loggingService: LoggingService) { }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    // Then the service can be used normally.
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
