5a0c2e1e109aa5082950936148dbc19d5f9cc14b
Henrique Baron <henrique.baron@gmail.com>
2022-05-17 18:07:53 +0200

    Note on Subscription: unsubscribing

--- routing/src/app/users/user/user.component.ts
+++ routing/src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription = undefined;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -19,12 +21,20 @@ export class UserComponent implements OnInit {
       name: this.route.snapshot.params['name']
     };
     /* But the Route.params property is an Observable, and let us subscribe to changes. */
-    this.route.params.subscribe((params: Params) => {
+    this.paramsSubscription = this.route.params.subscribe((params: Params) => {
       this.user.id = params['id'];
       this.user.name = params['name'];
     })
     /* Unless sure that the route will not change within the component itself, it is recommended to
     use the subscribe() method to the Route.params. */
+  }
+
+  /* A note on good practice: The subscription used in the ngOnInit() method is not destroyed by the
+  TypeScript interpreter automatically. It will reside in memory unless you unsubscribe.
+  Angular does that automatically though. But still, for a sake of good practice, we were implementing
+  OnDestroy() to unsubscribe manually. */
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
