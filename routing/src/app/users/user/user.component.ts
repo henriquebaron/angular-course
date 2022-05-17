49f8c673cf86d7b153ee6b7a62db608a46fde5e7
Henrique Baron <henrique.baron@gmail.com>
2022-05-17 18:02:17 +0200

    Routes: use case of changing the route within the component

--- routing/src/app/users/user/user.component.ts
+++ routing/src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -13,9 +13,18 @@ export class UserComponent implements OnInit {
 
   ngOnInit() {
     this.user = {
+      /* Previously, only a "snapshot" of the route was being taken, therefore not updating the user
+      object after the component was initialized */
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
+    /* But the Route.params property is an Observable, and let us subscribe to changes. */
+    this.route.params.subscribe((params: Params) => {
+      this.user.id = params['id'];
+      this.user.name = params['name'];
+    })
+    /* Unless sure that the route will not change within the component itself, it is recommended to
+    use the subscribe() method to the Route.params. */
   }
 
 }
