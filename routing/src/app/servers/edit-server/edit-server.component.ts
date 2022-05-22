f8121b4f5bb6f27eba27868716e09ef1e8e59e42
Henrique Baron <henrique.baron@gmail.com>
2022-05-22 14:39:22 +0200

    Routing: Reading query parameters and fragment on the code

--- routing/src/app/servers/edit-server/edit-server.component.ts
+++ routing/src/app/servers/edit-server/edit-server.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { timeStamp } from 'console';
 
 import { ServersService } from '../servers.service';
 
@@ -12,9 +14,16 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    /* Just as demonstrated before, for the query parameters and the fragment there is
+    also the possibility of reading only the snapshot, or subscribing to changes in these
+    properties, which is usually the recommended approach. */
+    console.log(this.route.snapshot.queryParams);
+    console.log(this.route.snapshot.fragment);
+    // this.route.queryParams.subscribe();
+    // this.route.fragment.subscribe();
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
