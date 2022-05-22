7e2440699feb7f3f91e1697b8a38f37b9dd2d060
Henrique Baron <henrique.baron@gmail.com>
2022-05-22 15:03:34 +0200

    Routing: navigate through different servers with the server ID

--- routing/src/app/servers/server/server.component.ts
+++ routing/src/app/servers/server/server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -10,10 +11,16 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    // Note: "+" works as a type cast operator (to number)
+    const id: number = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    this.route.params.subscribe((params: Params) => {
+      const id: number = +params['id'];
+      this.server = this.serversService.getServer(id);
+    })
   }
 
 }
