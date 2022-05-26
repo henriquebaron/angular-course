a9345146e98203f5b68cb8071d9e78c6e46448a6
Henrique Baron <henrique.baron@gmail.com>
2022-05-26 12:20:36 +0200

    Routing: create a fake login function (to test canActivate)

--- routing/src/app/home/home.component.ts
+++ routing/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
   }
@@ -18,6 +19,14 @@ export class HomeComponent implements OnInit {
     /* The "navigationExtras" object can inlude the "queryParams" and "fragment" properties as well, which
     also resolves to the address /servers/<id>/edit?allowEdit=1#loading */
     this.router.navigate(['/servers', id, 'edit'], {queryParams: { allowEdit: '1'}, fragment: 'loading'});
+  }
+
+  onLogin(): void {
+    this.authService.login();
+  }
+
+  onLogout(): void {
+    this.authService.logout();
   }
 
 }
