0abb9c7edc0a52e9f52ad824ab3590a3fa57212a
Henrique Baron <henrique.baron@gmail.com>
2022-06-07 21:54:23 +0200

    Add child routes to edit and create recipes

--- recipes-app/src/app/app-routing.module.ts
+++ recipes-app/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent, children: [
     { path: '', component: RecipeStartComponent },
-    { path: ':id', component: RecipeDetailComponent }
+    // Order is important! ":id" before "new" does not work!
+    { path: 'new', component: RecipeEditComponent },
+    { path: ':id', component: RecipeDetailComponent },
+    { path: ':id/edit', component: RecipeEditComponent }
   ] },
   { path: 'shopping-list', component: ShoppingListComponent }
 ];
