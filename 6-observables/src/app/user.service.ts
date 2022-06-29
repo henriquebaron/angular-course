6bdffb377ac1b0300366144e64cd99368cce4a34
Henrique Baron <henrique.baron@gmail.com>
2022-06-29 20:06:41 +0200

    Observables: implement Subject
    Adapting the code created in the last commit.

--- 6-observables/src/app/user.service.ts
+++ 6-observables/src/app/user.service.ts
@@ -1,6 +1,13 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class UserService {
-	activatedEmitter = new EventEmitter<boolean>();
+	/* Although the Subject works really similarly to the EventEmitter, it should be
+	preferred over the latter.
+	The Subject is suited for the Observable communication where the event has to be
+	actively triggered somewhere else, like here, where it is triggered on the
+	UserComponent.
+	The EventEmitter still must be used when using the @Output decorator. */
+	activatedEmitter = new Subject<boolean>();
 }
