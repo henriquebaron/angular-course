1643b797f5d86bc7f6023155ae09a2190dd20f02
Henrique Baron <henrique.baron@gmail.com>
2022-06-29 21:09:12 +0200

    Forms TD: Changing form values programatically

--- 7-forms-template/src/app/app.component.ts
+++ 7-forms-template/src/app/app.component.ts
@@ -16,6 +16,24 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    /* Two methods can be used to change the value of the form elements: NgForm.setValue and NgForm.patchValue. 
+     * The former replaces the whole "value" property of the form with a copy of the object, which means that all the value
+     * properties have to be written at once.
+     * The latter allows to change single values, like done in the uncommented lines below. */
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm): void {
