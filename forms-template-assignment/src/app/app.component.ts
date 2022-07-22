b7738d3898b58c1198d2649225e9cbf4a896380d
Henrique Baron <henrique.baron@gmail.com>
2022-07-22 11:08:35 +0200

    Assignment 6: Resolve assignment

--- forms-template-assignment/src/app/app.component.ts
+++ forms-template-assignment/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('userForm') formData: NgForm;
+  defaultSubscription: string = 'advanced';
+
+  onSubmit(): void {
+    console.log(this.formData);
+  }
 
 }
