bb7ced76ae7fa8a25beb366e5599c6ced440df6d
Henrique Baron <henrique.baron@gmail.com>
2022-08-04 13:30:17 +0200

    CP: Allow edit elements of the shopping list

--- recipes-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipes-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -30,9 +30,15 @@ export class ShoppingEditComponent implements OnInit {
     });
   }
 
-  onAddIngredient(form: NgForm): void {
+  onSubmit(form: NgForm): void {
     const newIngredient = new Ingredient(form.value.name, form.value.amount);
-    this.shoppingListService.addIngredient(newIngredient);
+    if (this.editMode) {
+      this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient);
+    } else {
+      this.shoppingListService.addIngredient(newIngredient);
+    }
+    this.shoppingListForm?.reset();
+    this.editMode = false;
   }
 
 }
