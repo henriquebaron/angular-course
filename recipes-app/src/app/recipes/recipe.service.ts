527d32f257d482c24dd71dc136b5ac82d46c2ca1
Henrique Baron <henrique.baron@gmail.com>
2022-08-05 11:48:46 +0200

    CP: Creating and editing recipes

--- recipes-app/src/app/recipes/recipe.service.ts
+++ recipes-app/src/app/recipes/recipe.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe('Test Recipe', 'This is just a test!', "../../../assets/images/limoncello.jpg", [
@@ -30,6 +32,16 @@ export class RecipeService {
 
   addIngredientsToShoppingList(recipe: Recipe): void {
     this.shoppingListService.addMultipleIngredients(recipe.ingredients);
+  }
+
+  addRecipe(recipe: Recipe): void {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe): void {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
   }
 
 }
