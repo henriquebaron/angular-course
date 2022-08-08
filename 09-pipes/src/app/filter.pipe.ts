40eb9b0525b405db0f35dc7a4ef45230d56a6c56
Henrique Baron <henrique.baron@gmail.com>
2022-08-08 17:03:44 +0200

    Pipes: pure and impure pipes

--- 09-pipes/src/app/filter.pipe.ts
+++ 09-pipes/src/app/filter.pipe.ts
@@ -1,8 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { resourceLimits } from 'worker_threads';
 
+/* In the previous state (commit), the filter would not work if you filter for 'stable'
+ * and then clicked to add new servers. That's because the "pure" Angular pipes are only
+ * re-evaluated when on input changes, and not on any data change. This is by design, in
+ * order not to create performance issues. But for demonstration, this behavior can be
+ * forced by making "pure" as false. */
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  pure: false
 })
 export class FilterPipe implements PipeTransform {
 
