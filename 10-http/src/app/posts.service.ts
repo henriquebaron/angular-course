69f4986083020206f4e4f02b6c0f8761a927edd8
Henrique Baron <henrique.baron@gmail.com>
2022-08-15 16:58:43 +0200

    Http: Configuring what to observe on the request

--- 10-http/src/app/posts.service.ts
+++ 10-http/src/app/posts.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Subject, throwError } from "rxjs";
-import { map, catchError } from "rxjs/operators";
+import { map, catchError, tap } from "rxjs/operators";
 import { Post } from "./post.model";
 
 @Injectable({
@@ -19,26 +19,33 @@ export class PostsService {
     const postData: Post = { title: title, content: content };
     // Send Http request
     this.http
-      .post<{ name: string }>(this.url + "posts.json", postData)
-      .subscribe((responseData) => {
-        // HTTP requests are only sent if the subscribe function is called. Otherwise Angular considers that it's not
-        // necessary to make the request, since "nobody's interested".
-        console.log(responseData);
-      }, error => {
-        this.error.next(error.message);
-      });
+      .post<{ name: string }>(this.url + "posts.json", postData, {
+        // This argument allows for better control on what to observe: the default would be "body".
+        // Here, instead, I will request to observe the whole response (which contains the body).
+        // observe: 'body'
+        observe: "response",
+      })
+      .subscribe(
+        (responseData) => {
+          // HTTP requests are only sent if the subscribe function is called. Otherwise Angular considers that it's not
+          // necessary to make the request, since "nobody's interested".
+          console.log(responseData); // So, to keep it working like before, I would have to access the ".body" element of the response
+        },
+        (error) => {
+          this.error.next(error.message);
+        }
+      );
   }
 
   fetchPosts() {
     let queryParams = new HttpParams();
-    queryParams = queryParams.append('print', 'pretty')
-    queryParams = queryParams.append('custom', 'key') // Several query parameters can be added this way (this second is just an example, has no effect)
+    queryParams = queryParams.append("print", "pretty");
+    queryParams = queryParams.append("custom", "key"); // Several query parameters can be added this way (this second is just an example, has no effect)
     return this.http
-      .get<{ [key: string]: Post }>(this.url + "posts.json",
-      {
-        headers: new HttpHeaders({ 'Customer-Header': 'Hello' }), // This allows to add any custom header that is needed
+      .get<{ [key: string]: Post }>(this.url + "posts.json", {
+        headers: new HttpHeaders({ "Customer-Header": "Hello" }), // This allows to add any custom header that is needed
         // params: new HttpParams().set('print', 'pretty') // This is an alternative way to set the parameters directly.
-        params: queryParams
+        params: queryParams,
       })
       .pipe(
         map((responseData) => {
@@ -51,7 +58,7 @@ export class PostsService {
           }
           return postsArray;
         }),
-        catchError(errorRes => {
+        catchError((errorRes) => {
           // I could do any error processing here: log it to a server etc.
           return throwError(errorRes);
         })
@@ -59,6 +66,22 @@ export class PostsService {
   }
 
   deleteAllPosts() {
-    return this.http.delete(this.url + "posts.json");
+    return this.http
+      .delete(this.url + "posts.json", {
+        // Here, I am subscribing to the events, which are being processed below.
+        observe: "events",
+      })
+      .pipe(
+        tap((event) => {
+          // The tap operator allows to do something with the object on the pipe, but without altering it.
+          console.log(event);
+          if (event.type === HttpEventType.Sent) {
+            // ...
+          }
+          if (event.type === HttpEventType.Response) {
+            console.log(event.body);
+          }
+        })
+      );
   }
 }
