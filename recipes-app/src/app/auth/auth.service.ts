e932ce09c36b0fd52cb801ef5ecfc45961a07f07
Henrique Baron <henrique.baron@gmail.com>
2022-08-25 14:22:10 +0200

    CP-Auth: Save token in the browser Local Storage
    And implement auto-login functionality with it.

--- recipes-app/src/app/auth/auth.service.ts
+++ recipes-app/src/app/auth/auth.service.ts
@@ -71,6 +71,21 @@ export class AuthService {
       );
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData') ?? ''); // Null coalescing operator, since I'm working in 'strict' mode
+    if (!userData) return;
+    const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
@@ -102,5 +117,6 @@ export class AuthService {
     const expirationDate: Date = new Date(new Date().getTime() + expiresIn);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 }
