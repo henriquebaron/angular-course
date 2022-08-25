d4cc890abf9ee0d39c9e6cca1f2020a4e9bb680a
Henrique Baron <henrique.baron@gmail.com>
2022-08-25 14:22:10 +0200

    CP-Auth: Send signup request to Firebase API

--- recipes-app/src/app/auth/auth.component.ts
+++ recipes-app/src/app/auth/auth.component.ts
@@ -1,26 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit {
   isLoginMode: boolean = true;
 
-  constructor() { }
+  constructor(private auth: AuthService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
-    form.reset();
-  }
+    if (form.valid) {
+      const email = form.value.email;
+      const password = form.value.password;
 
+      if (!this.isLoginMode) {
+        this.auth.signUp(email, password).subscribe(
+          (resData) => {
+            console.log(resData);
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+      }
+      form.reset();
+    }
+  }
 }
