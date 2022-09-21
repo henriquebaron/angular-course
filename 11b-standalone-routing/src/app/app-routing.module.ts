2dbefa632f9f43936ccb6ed3f6d09f53ae2473da
Henrique Baron <henrique.baron@gmail.com>
2022-09-21 17:37:38 +0200

    Standalone: Lazy loading a single standalone component

--- 11b-standalone-routing/src/app/app-routing.module.ts
+++ 11b-standalone-routing/src/app/app-routing.module.ts
@@ -11,10 +11,19 @@ const routes: Route[] = [
   },
   {
     path: 'about',
-    component: AboutComponent,
+    /* Introduced in Angular 14: loading a single standalone component lazily
+     * with the "loadComponent" function. Note the class is a "Component" */
+    // component: AboutComponent,
+    loadComponent: () => 
+      import('./about/about.component').then(
+        (mod) => mod.AboutComponent
+      )
   },
   {
     path: 'dashboard',
+    /* Classic way of lazy loading routes: A whole module with the 
+     * desired routes is loaded with the "loadChildren" function. Note
+     * that the class is a "Module" */
     loadChildren: () =>
       import('./dashboard/dashboard-routing.module').then(
         (mod) => mod.DashboardRoutingModule
