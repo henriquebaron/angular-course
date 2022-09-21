4eccce38a9b0b9bd7eaa1a817c4b562f230c3135
Henrique Baron <henrique.baron@gmail.com>
2022-09-21 17:37:38 +0200

    Standalone: Convert WelcomeComponent to standalone

--- 11-standalone-components/src/app/app.module.ts
+++ 11-standalone-components/src/app/app.module.ts
@@ -2,17 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { DetailsComponent } from './welcome/details/details.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   /* From the moment that I turn a component into standalone, it *cannot* be
    * present in the "declarations" anymore. But since the standalone component
-   * is not being imported anywhere (i.e. in the WelcomeComponent), it has to
+   * is not being imported anywhere, it has to
    * be present among the "imports" of the AppModule */
   // declarations: [AppComponent, WelcomeComponent, DetailsComponent],
-  declarations: [AppComponent, WelcomeComponent],
-  imports: [BrowserModule, DetailsComponent],
+  declarations: [AppComponent],
+  imports: [BrowserModule, WelcomeComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
--- 11-standalone-components/src/app/welcome/welcome.component.ts
+++ 11-standalone-components/src/app/welcome/welcome.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-// import { DetailsComponent } from './details/details.component';
+import { DetailsComponent } from './details/details.component';
 
-/* For the new standalone "DetailsComponent" to be loaded, it has to
- * declared as an import to the component which is calling it. But this
- * only works if the calling component is also standalone.
- * That is not the case for the WelcomeComponent as of now. */
+/* For the standalone "DetailsComponent" to be loaded, it has to
+ * declared as an import to the component which is calling it. */ 
 @Component({
-  // imports: [DetailsComponent],
+  standalone: true,
+  imports: [DetailsComponent],
   selector: 'app-welcome',
   templateUrl: './welcome.component.html'
 })
