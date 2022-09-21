4d14ba3d126a292eb4d966196400f1797fede4a2
Henrique Baron <henrique.baron@gmail.com>
2022-09-21 17:37:38 +0200

    Standalone: Convert regular component to standalone
    But preserving the rest of the code in the traditional way (as if this was a migration process).

--- 11-standalone-components/src/app/app.module.ts
+++ 11-standalone-components/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 import { DetailsComponent } from './welcome/details/details.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent, DetailsComponent],
-  imports: [BrowserModule, SharedModule],
+  /* From the moment that I turn a component into standalone, it *cannot* be
+   * present in the "declarations" anymore. But since the standalone component
+   * is not being imported anywhere (i.e. in the WelcomeComponent), it has to
+   * be present among the "imports" of the AppModule */
+  // declarations: [AppComponent, WelcomeComponent, DetailsComponent],
+  declarations: [AppComponent, WelcomeComponent],
+  imports: [BrowserModule, DetailsComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
--- 11-standalone-components/src/app/welcome/welcome.component.ts
+++ 11-standalone-components/src/app/welcome/welcome.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
+// import { DetailsComponent } from './details/details.component';
 
+/* For the new standalone "DetailsComponent" to be loaded, it has to
+ * declared as an import to the component which is calling it. But this
+ * only works if the calling component is also standalone.
+ * That is not the case for the WelcomeComponent as of now. */
 @Component({
+  // imports: [DetailsComponent],
   selector: 'app-welcome',
   templateUrl: './welcome.component.html'
 })
