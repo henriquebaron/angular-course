77133938f15c560f024904cfe6311fc846e24e21
Henrique Baron <henrique.baron@gmail.com>
2022-09-28 17:33:38 +0200

    NgRx: Subscribe to the execution of an action

--- recipes-app/src/app/recipes/recipes-resolver.service.ts
+++ recipes-app/src/app/recipes/recipes-resolver.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import * as fromApp from '../store/app.reducer';
 import * as RecipeActions from './store/recipe.actions';
-import { DataStorageService } from '../shared/data-storage.service';
 import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
 import { Store } from '@ngrx/store';
+import { Actions, ofType } from '@ngrx/effects';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesResolverService implements Resolve<Recipe[]> {
 
-  constructor(private dataStorageService: DataStorageService, private recipeService: RecipeService, private store: Store<fromApp.AppState>) { }
+  constructor(private recipeService: RecipeService, private store: Store<fromApp.AppState>, private actions$: Actions) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Recipe[] | Observable<Recipe[]> | Promise<Recipe[]> {
-    const recipes = this.recipeService.getRecipes();
-     if (recipes.length === 0) {
-      return this.dataStorageService.fetchRecipes();
-     } else {
-      return recipes;
-     }
+    // const recipes = this.recipeService.getRecipes();
+    //  if (recipes.length === 0) {
+    //   return this.dataStorageService.fetchRecipes();
+    //  } else {
+    //   return recipes;
+    //  }
+
+    /* It is also possible to use the Actions class outside the Effects class I created. An example is here, where I subscribe
+     * to the execution of a specific action.
+     * The 'take' operator is used to unsubscribe automatically after receiving one callback. */
+    this.store.dispatch(new RecipeActions.FetchRecipes());
+    return this.actions$.pipe(
+      ofType(RecipeActions.SET_RECIPES),
+      take(1)
+    );
   }
 }
