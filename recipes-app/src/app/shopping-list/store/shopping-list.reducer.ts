eb8d4f7a903a61235af7c9635f679d2b4be30280
Henrique Baron <henrique.baron@gmail.com>
2022-09-28 17:33:38 +0200

    NgRx: Create first Reducer
    Still not functional.

---
+++ recipes-app/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -0,0 +1,24 @@
+import { Ingredient } from "../../shared/ingredient.model";
+import * as ShoppingListActions from "./shoppint-list.actions";
+
+const initialState = {
+  ingredients: [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ],
+};
+
+/* In the Redux pattern, the state is immutable (read-only), and changes are made
+ * with pure functions only -- functions that receive input and produce output *without
+ * changing the inputs*.
+ * That is why a new object is returned, and the spread operator is used in the returned
+ * object to include a copy of the previous state. */
+export function shoppingListReducer(state = initialState, action: ShoppingListActions.AddIngredient) {
+  switch(action.type) {
+    case ShoppingListActions.ADD_INGREDIENT: // By convention, action types are written uppercase
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload]
+      }
+  }
+}
