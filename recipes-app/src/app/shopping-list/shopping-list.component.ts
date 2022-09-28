7dcad636967d4e3bd3eed9814b5622fd586ad7fa
Henrique Baron <henrique.baron@gmail.com>
2022-09-28 17:33:38 +0200

    NgRx: Remove the shopping list service
    And all its references. It has been completely replaced by the NgRx Store.

--- recipes-app/src/app/shopping-list/shopping-list.component.ts
+++ recipes-app/src/app/shopping-list/shopping-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
 import { Ingredient } from 'src/app/shared/ingredient.model';
-import { ShoppingListService } from './shopping-list.service';
 import * as fromShoppingList from './store/shopping-list.reducer';
 import * as ShoppingListActions from './store/shopping-list.actions';
 
