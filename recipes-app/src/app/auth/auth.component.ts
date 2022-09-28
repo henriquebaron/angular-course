9c9364b654c1d35a8281ea163071eb8c6818dd75
Henrique Baron <henrique.baron@gmail.com>
2022-09-28 17:33:38 +0200

    NgRx: Clean up the AuthComponent committed
    Which does not need the AuthService anymore.

--- recipes-app/src/app/auth/auth.component.ts
+++ recipes-app/src/app/auth/auth.component.ts
@@ -6,11 +6,9 @@ import {
   ViewChild,
 } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
-import { Observable, Subject, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { AlertComponent } from '../shared/alert/alert.component';
 import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
-import { AuthResponseData, AuthService } from './auth.service';
 import { Store } from '@ngrx/store';
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from './store/auth.actions';
@@ -32,14 +30,10 @@ export class AuthComponent implements OnInit, OnDestroy {
   private closeSub: Subscription = new Subscription();
 
   // constructor(private auth: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver) {}
-  constructor(
-    private auth: AuthService,
-    private router: Router,
-    private store: Store<fromApp.AppState>
-  ) {}
+  constructor(private store: Store<fromApp.AppState>) {}
 
   ngOnInit(): void {
-    this.store.select('auth').subscribe(authState => {
+    this.store.select('auth').subscribe((authState) => {
       this.isLoading = authState.loading;
       this.error = authState.authError;
       if (this.error) this.showErrorAlert(this.error);
@@ -55,30 +49,17 @@ export class AuthComponent implements OnInit, OnDestroy {
       const email = form.value.email;
       const password = form.value.password;
 
-      let authObservable: Observable<AuthResponseData>;
-
       this.isLoading = true;
       if (this.isLoginMode) {
-        // authObservable = this.auth.login(email, password)
-        this.store.dispatch(new AuthActions.LoginStart({ email: email, password: password }));
+        this.store.dispatch(
+          new AuthActions.LoginStart({ email: email, password: password })
+        );
       } else {
-        // authObservable = this.auth.signUp(email, password);
-        this.store.dispatch(new AuthActions.SignupStart({ email: email, password: password }));
+        this.store.dispatch(
+          new AuthActions.SignupStart({ email: email, password: password })
+        );
       }
 
-      // authObservable.subscribe(
-      //   (resData) => {
-      //     console.log(resData);
-      //     this.isLoading = false;
-      //     this.router.navigate(['/recipes']);
-      //   },
-      //   (errorMessage) => {
-      //     console.log(errorMessage);
-      //     this.error = errorMessage;
-      //     this.showErrorAlert(errorMessage);
-      //     this.isLoading = false;
-      //   }
-      // );
       form.reset();
     }
   }
