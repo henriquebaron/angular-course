97bab0fbe7c9cea5eb4bafb2b843f75c0b87719a
Henrique Baron <henrique.baron@gmail.com>
2022-09-28 17:33:38 +0200

    NgRx: Action identifiers must be unique
    So the code was refactored with feature name prefixes to avoid this in the future.

--- recipes-app/src/app/shopping-list/store/shopping-list.actions.ts
+++ recipes-app/src/app/shopping-list/store/shopping-list.actions.ts
@@ -1,12 +1,18 @@
 import { Action } from "@ngrx/store";
 import { Ingredient } from "src/app/shared/ingredient.model";
 
-export const ADD_INGREDIENT = 'ADD_INGREDIENT';
-export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
-export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
-export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
-export const START_EDIT = 'START_EDIT';
-export const STOP_EDIT = 'STOP_EDIT';
+/* Important thing about reducers: every action is dispatched through ALL REDUCERS
+ * of the application. For example, actions dispacthed to the Auth reducer also pass
+ * by here.
+ * For this reason, the value of the Action type, defined by these constants, must be
+ * unique in the application. So, it is a good practice to create these constants with
+ * a "feature prefix", to avoid type identifier clashes. */
+export const ADD_INGREDIENT = '[Shopping List] Add Ingredient'
+export const ADD_INGREDIENTS = '[Shopping List] Add Ingredients';
+export const UPDATE_INGREDIENT = '[Shopping List] Update Ingredient';
+export const DELETE_INGREDIENT = '[Shopping List] Delete Ingredient';
+export const START_EDIT = '[Shopping List] Start Edit';
+export const STOP_EDIT = '[Shopping List] Stop Edit';
 
 export class AddIngredient implements Action {
   readonly type: string = ADD_INGREDIENT;
