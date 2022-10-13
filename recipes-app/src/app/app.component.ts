409ad11b2820a33c0ce5f80b3f65ef4074a53811
Henrique Baron <henrique.baron@gmail.com>
2022-10-13 13:31:21 +0200

    Universal: Check for browser environment to run autologin

--- recipes-app/src/app/app.component.ts
+++ recipes-app/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as fromApp from './store/app.reducer';
 import * as AuthActions from './auth/store/auth.actions';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,16 @@ import * as AuthActions from './auth/store/auth.actions';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private store: Store<fromApp.AppState>) { }
+  constructor(private store: Store<fromApp.AppState>, @Inject(PLATFORM_ID) private platformId) { }
 
   ngOnInit(): void {
     // this.authService.autoLogin();
-    this.store.dispatch(new AuthActions.AutoLogin());
+    /* Because of Angular Universal, the app is initially run in the NodeJS server. The Login functionality
+     * calls 'localStorage' functions though, which are only available in the browser API. For that reason,
+     * I must check if the application is running in a browser before calling the auto login action. */
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(new AuthActions.AutoLogin());
+    }
   }
 
 }
