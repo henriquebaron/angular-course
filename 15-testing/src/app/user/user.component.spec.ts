d356a31aef70542ffd220f828c820c02b5de87e1
Henrique Baron <henrique.baron@gmail.com>
2022-10-17 17:24:23 +0200

    Testing: Create tests for the User Component

--- 15-testing/src/app/user/user.component.spec.ts
+++ 15-testing/src/app/user/user.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
+import { UserService } from './user.service';
 
 describe('UserComponent', () => {
   let component: UserComponent;
@@ -22,5 +23,32 @@ describe('UserComponent', () => {
     let app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  // Test that accesses the injector and uses a service injected into the component
+  it('should use the user name from the service', () => {
+    let fixture = TestBed.createComponent(UserComponent);
+    let app = fixture.debugElement.componentInstance;
+    let userService = fixture.debugElement.injector.get(UserService);
+    fixture.detectChanges();
+    expect(userService.user.name).toEqual(app.user.name);
+  });
+
+  // Below two tests that assess the HTML template code.
+  it('should display the user name if the user is logged in', () => {
+    let fixture = TestBed.createComponent(UserComponent);
+    let app = fixture.debugElement.componentInstance;
+    app.isLoggedIn = true;
+    fixture.detectChanges();
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p').textContent).toContain(app.user.name);
+  })
+
+  it('should not display the user name if the user is not logged in', () => {
+    let fixture = TestBed.createComponent(UserComponent);
+    let app = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p').textContent).not.toContain(app.user.name);
+  })
 
 });
