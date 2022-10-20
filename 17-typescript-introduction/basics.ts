f90816fb65700a19a81a06c480abf447f10ff76b
Henrique Baron <henrique.baron@gmail.com>
2022-10-20 13:16:42 +0200

    TypeScript: Union types and type aliases

--- 17-typescript-introduction/basics.ts
+++ 17-typescript-introduction/basics.ts
@@ -27,3 +27,25 @@ person = {
     // Object form is not flexible anymore: code breaks with the extra property below
     // test: false
 }
+
+
+// TypeScript can also infer types:
+let course = 'Angular - The Complete Guide';
+// course = 123; // Thus this line breaks
+
+// Feature that differs TypeScript *a lot* from other languages I knew: Union types
+// In this case, the second line does not break, because both types are allowed for that variable
+let otherCourse: string | number = 'Angular - The Complete Guide';
+otherCourse = 123;
+
+// type keyword: doesn't exist in JavaScript. Creates type aliases.
+type Person = {
+    name: string;
+    age: number;
+};
+
+// Variable declared with the 'Person' type alias
+let anotherPerson: Person = {
+    name: 'Henrique',
+    age: 29
+};
