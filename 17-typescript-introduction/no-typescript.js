381147798b1f0a72b8440cb346a98185873cdb9c
Henrique Baron <henrique.baron@gmail.com>
2022-10-20 13:16:42 +0200

    TypeScript: Add initial JS example

---
+++ 17-typescript-introduction/no-typescript.js
@@ -0,0 +1,8 @@
+function add(a, b) {
+  return a + b;
+}
+
+// Javascript: no static typing. Strings can be passed to this function and will then be concatenated.
+const result = add('2', '5');
+
+console.log(result);
